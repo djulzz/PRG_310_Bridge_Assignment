a70a744daf2a8711c909837715d286339e0980f0
mariojay13 <mariojay13@gmail.com>
2016-11-07 14:11:12 -0800

    Mario's Contrabution
    Mario's Contrabution

---
+++ GameEngine/GameEngine/DummyIRenderer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    public class DummyIRenderer:IRender
+    {
+        public void Render()
+        {
+            Console.WriteLine("Open Dummy Running");
+        }
+    }
+}
