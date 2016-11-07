35906b9dbee8463383dd2c288614aeac928d0051
jtori82 <jtori2713@gmail.com>
2016-11-07 14:10:55 -0800

    DirectXrender from John
    John's code

---
+++ GameEngine/GameEngine/DirectXRenderer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    class DirectXRenderer: IRender
+    {
+        public void Render()
+        {
+            Console.WriteLine("Open Direct X Running");
+        }
+    }
+}
