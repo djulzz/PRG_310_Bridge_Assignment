898d60d25c8a906066dce649e91b03779d9b6755
Julien Esposito <julien.esposito@gmail.com>
2016-11-07 13:55:59 -0800

    Pre - Loren
    Pre - Loren

---
+++ GameEngine/GameEngine/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    public class Game : IGame
+    {
+        private IRenderer m_renderer;
+
+        // Constructor
+        Game( IRenderer renderer )
+        {
+            m_renderer = renderer;
+        }
+
+        public void Render()
+        {
+            m_renderer.Render();
+        }
+    }
+}
